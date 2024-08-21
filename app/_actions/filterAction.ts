4d2d491467361648c0b40f68be300276f8bb230f
jfadgyas <djdeville72@yahoo.com>
2024-08-21 11:15:19 +0200

    filteraction2

--- app/_actions/filterAction.ts
+++ app/_actions/filterAction.ts
@@ -3,20 +3,21 @@
 const filterAction = (item: Record<string, any>, searchParams: { [key: string]: string | string[] | undefined } ) => {
        
     return Object.keys(item._doc).every(key => {
-        if (!searchParams[key]) return item
-        const newSearch = Array.isArray(searchParams[key]) ? searchParams[key] : [searchParams[key]]
-        if ((key === 'price' || key === 'forAge' || key === 'pieces') && searchParams[key]){
-            return newSearch.some(filter => {
-                const minPrice = filter!.split('-')[0]
-                const maxPrice = filter!.split('-')[1]
-                return item[key] >= +minPrice && item[key] <= +maxPrice
-            })
-        }
-        if (key === 'tags' && searchParams[key]){
-            return newSearch.some(filter => item[key].includes(filter))
-        }
-        if ((key === 'isOnSale' || key === 'make') && searchParams[key]){
-            return newSearch.some(filter => item[key].toString() === filter)
+        if (searchParams[key]){
+            const newSearch = Array.isArray(searchParams[key]) ? searchParams[key] : [searchParams[key]]
+            if ((key === 'price' || key === 'forAge' || key === 'pieces')){
+                return newSearch.some(filter => {
+                    const minPrice = filter!.split('-')[0]
+                    const maxPrice = filter!.split('-')[1]
+                    return item[key] >= +minPrice && item[key] <= +maxPrice
+                })
+            }
+            if (key === 'tags'){
+                return newSearch.some(filter => item[key].includes(filter))
+            }
+            if ((key === 'isOnSale' || key === 'make')){
+                return newSearch.some(filter => item[key].toString() === filter)
+            }
         }
         return item
     })
