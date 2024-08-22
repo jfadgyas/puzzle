cbe6aa5f873638c80566cd576f4b95898463046c
jfadgyas <djdeville72@yahoo.com>
2024-08-22 12:39:13 +0200

    font function in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import {Seymour_One, Merriweather_Sans, Poppins} from 'next/font/google'
+
 import type { Metadata } from "next";
 import "./globals.scss";
 
@@ -8,6 +10,26 @@ import StoreProvider from "./_context/context";
 import Toast from "./components/Toast";
 
 import { cookies } from "next/headers";
+
+const seymour = Seymour_One({
+  subsets: ['latin'],
+  weight: '400',
+  display: 'swap',
+  variable: '--font-seymour'
+})
+
+const merri = Merriweather_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-merri'
+})
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  display: 'swap',
+  variable: '--font-poppins'
+})
 
 export const metadata: Metadata = {
   title: "Puzzle Plaza - Jigsaw Puzzles for All Ages",
@@ -24,7 +46,7 @@ export default function RootLayout({
   const currentCookies = cookies().getAll()
   
   return (
-    <html lang="en">
+    <html lang="en" className={`${seymour.variable} ${merri.variable} ${poppins.variable}`}>
       <body className=''>
         <StoreProvider currentCookies={currentCookies}>
           <NavBar />
