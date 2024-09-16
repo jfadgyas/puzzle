a7df3c96f1a6f4e9203b620d3afcc68d767bd45a
jfadgyas <djdeville72@yahoo.com>
2024-09-16 17:37:47 +0200

    seo robots

--- app/robots.ts
+++ app/robots.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from "next";
 
 const baseUrl = 'https://puzzleplaza.netlify.app'
 
-export function robots(): MetadataRoute.Robots{
+export default function robots(): MetadataRoute.Robots{
 
     return {
         rules: {
